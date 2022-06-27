b8441829376a1b47e593b736575c4287797f4e2a
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2022-06-27 11:04:08 +0300

    Setting Up Categories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const categories = [
+    {
+      id : 1,
+      title : 'HATS'
+    },
+    {
+      id : 1,
+      title : 'SNEAKERS'
+    },
+    {
+      id : 1,
+      title : 'HATS'
+    },
+    {
+      id : 1,
+      title : 'MENS'
+    },
+    {
+      id : 1,
+      title : 'WOMENS'
+    }
+  ]
   return (
-    <div>Hello Ecommerce world</div>
+    <div className='categories-container'>
+     {
+      categories.map(({title}) => (
+      <div className='category-container'>
+      {/*<img></img>*/}
+        <div className='category-body-container'>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+
+      ))
+     }
+      
+    </div>
   );
 }
 
