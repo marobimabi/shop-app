844ae741140853769b601b7a01cfda627f420ea7
mahamat ali youssouf <56170343+marobimabi@users.noreply.github.com>
2022-07-13 20:54:28 +0300

    products by categorry

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Authentification from './components/home/authentification/authentificatio
 import Shop from './components/home/shop component/shop.component';
 import CheckOut from './components/home/checkOut component/checkout.component';
 
+
 const App = () => {
   return (
     <Routes>
 
       <Route path='/' element={<Navigation />} >
           <Route index element={<Home />} />
-          <Route path='shop' element={<Shop />} />
+          <Route path='shop/*' element={<Shop />} />
           <Route path='auth' element={<Authentification />} />
           <Route path='checkout' element={<CheckOut/>} />
       </Route>
--- src/components/home/shop component/shop.component.jsx
+++ src/components/home/shop component/shop.component.jsx
@@ -1,30 +1,21 @@
 
-import { Fragment, useContext } from 'react';
-import { CategoriesContext } from '../../../contexts/context component/categories.context';
-import ProductCard from '../../products cards/products-car.component';
+import { Route, Routes } from 'react-router-dom';
+import Category from '../category/category.component';
+
+import CategoriesPreview from '../categries preview/categories-preview.component';
+
+
 import './shop.styles.scss'
 
 const Shop = () =>{
-    const {categoriesMap} = useContext(CategoriesContext);
+   
 
     return(
-        <Fragment>
+       <Routes>
+        <Route index element={<CategoriesPreview />} />
+        <Route path=":categry" element={<Category/>}/>
 
-            {
-                Object.keys(categoriesMap).map(title =>(
-                    <Fragment key={title}>
-                        <h1>{title}</h1>
-                        <div className='products-container'>
-                            {
-                                categoriesMap[title].map((product) => (  <ProductCard key={product.id}  product={product} /> ))
-                            }
-                        </div>
-                    </Fragment>
-                ))
-            }
-            
-        </Fragment>
-        
+       </Routes>
     );
 }
 
